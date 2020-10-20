357ec84cc6de32b4aa950eeb1120dedad4e350c2
CrisRubio <al375866@uji.es>
2020-10-20 23:25:07 +0200

    proyectoFinal

---
+++ Lab/P1/Proyecto/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Citrónico</title>
+	<META name="Author" content="AlumnoXXX">
+	<link rel="stylesheet" href="./css/estilo.css" type="text/css">
+	
+
+</head>
+
+
+<body>
+	<header>
+		<img src="logoProyecto.png" id="logo" alt="logo" />
+	</header>
