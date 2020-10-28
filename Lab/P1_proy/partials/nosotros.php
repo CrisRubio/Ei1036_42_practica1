d9e3c5de3a09517c24c4fe394835e1b696cd11b8
CrisRubio <al375866@uji.es>
2020-10-28 22:53:19 +0100

    nosotros

--- Lab/P1_proy/partials/nosotros.php
+++ Lab/P1_proy/partials/nosotros.php
@@ -10,5 +10,21 @@
                 Cetrónico es una tienda de comercio electrónico
             </td>
         </tr>
-    </table>
-    <br></br>
+        <tr>
+            <td class="teamFirst">
+                <br>
+                <b><i>EQUIPO : </i></b><i>Cristina Rubio Juarez</i>
+            </td>
+            <td class="team">
+                <br>
+                <img class="imgTeam" src="./img/team.jpg"/>
+            </td>
+        </tr>
+</table>
+
+<hr/>
+<h2>PREGUNTAS FRECUENTES</h2>
+<ul>
+    <li>¿ Cómo se realiza la compra de los productos ?</li>
+</ul>
+<br></br>
