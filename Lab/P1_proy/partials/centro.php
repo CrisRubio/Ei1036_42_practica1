e4bc6762dd624c0d58e9cababd258549fb65581b
CrisRubio <al375866@uji.es>
2020-10-29 19:14:09 +0100

    centro

--- Lab/P1_proy/partials/centro.php
+++ Lab/P1_proy/partials/centro.php
@@ -1,18 +1,28 @@
 
 <main>
-    <h1 class="centro">¡BIENVENIDOS A CETRÓNICO!</h1>
-    <br>
-    <button class="equipo">Equipo</button>
-    <br>
-    <br>
-    <button class="compra">Comprar</button>
-    <br>
-    <br>
-    <button class="socio">Hacerse socio</button>
-    <br>
-    <br>
-    <button class="inicioSesion">Iniciar sesión</button>
-    <br>
-    <br>
-    <br>
+
+<h1 class="centro">¡BIENVENIDOS A CETRÓNICO!</h1>
+<br>
+<br>
+
+<table>
+    <tr>
+        <td>Equipo</td>
+        <td class="izquierda"><button>Equipo</button></td>
+    </tr>
+    <tr>
+        <td><button>Comprar</button></td>
+        <td>Comprar</td>
+    </tr>
+    <tr>
+        <td>Registrarse</td>
+        <td class="izquierda"><button>Registrarse</button></td>
+    </tr>
+    <tr>
+        <td><button>Iniciar sesión</button></td>
+        <td>Iniciar sesion</td>
+    </tr>
+</table>
+<br>
+<br>
 </main>
